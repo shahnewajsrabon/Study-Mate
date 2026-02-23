87a6efd94edb609defef34001793824e5c209f83
Study Tracker <channel.data.transfer@gmail.com>
2026-02-23 20:16:08 +0600

    Update src/main.tsx and tsconfig.app.json

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 
 import ErrorBoundary from './shared/components/ui/ErrorBoundary.tsx'
 
-import { AuthProvider } from './shared/context/AuthContext.tsx'
 import { ThemeProvider } from './shared/context/ThemeContext.tsx'
 import { SoundProvider } from './shared/context/SoundContext.tsx'
 
@@ -13,13 +12,11 @@ import { SoundProvider } from './shared/context/SoundContext.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <AuthProvider>
-        <ThemeProvider>
-          <SoundProvider>
-            <App />
-          </SoundProvider>
-        </ThemeProvider>
-      </AuthProvider>
+      <ThemeProvider>
+        <SoundProvider>
+          <App />
+        </SoundProvider>
+      </ThemeProvider>
     </ErrorBoundary>
   </React.StrictMode>,
 )
